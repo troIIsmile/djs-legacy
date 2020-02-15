dac16a24fa1771adee17e4ea92b5b93b3b44f4e7
Jack <29169102+Jack5079@users.noreply.github.com>
2020-02-15 18:07:17 -0500

    fix tons of bugs

--- commands/help.ts
+++ commands/help.ts
@@ -4,10 +4,9 @@ export default (message: Message, _: Array<String>, bot: Bot) => {
   message.channel.send(
     Object.keys(bot.commands) // list of command names
       .map(e => '-' + e) // add "-" to the start
-      .map(e => `${e} | ${ bot.commands[ e.replace('-', '') ].desc }`)
-        .join('\n') // string seperated by newline
-      ,
-      { code: '' })
+      .join('\n') // string seperated by newline
+    ,
+    { code: '' })
 }
 
 
