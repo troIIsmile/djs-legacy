58edbf5975fd05ea3d2c036292b3a32882232708
Jack <29169102+Jack5079@users.noreply.github.com>
2020-02-17 16:21:41 -0500

    no timeout

--- commands/say.ts
+++ commands/say.ts
@@ -3,7 +3,7 @@ import { Message } from 'jackbot-discord'
 export default (message: Message, args: string[]) => {
   if (message.guild && message.guild.me.hasPermission('MANAGE_MESSAGES')) {
     message.channel.send(args.join(' '))
-    message.delete()
+    message.delete(0)
   } else if (message.guild) { message.channel.send('I don\'t have permission to delete messages! Give me Manage Messages!') }
   else message.channel.send('This is a DM, I can\'t delete your message.')
 }
