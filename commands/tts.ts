85aa23e3079beaad3a404b0ab2959ab3af61e323
Jack <29169102+Jack5079@users.noreply.github.com>
2020-02-16 23:38:18 -0500

    add `--slow`

--- commands/tts.ts
+++ commands/tts.ts
@@ -2,12 +2,21 @@ import { Message } from 'discord.js'
 import tts = require('google-tts-api')
 export default async (message: Message, args: string[]) => {
   try {
-    message.channel.send({
-      files: [{
-        attachment: await tts(args.join(' ')),
-        name: 'sound.mp3'
-      }]
-    })
+    if (args.filter(arg => arg.startsWith('--')).includes('--slow')) {
+      message.channel.send({
+        files: [ {
+          attachment: await tts(args.filter(arg => !arg.startsWith('--')).join(' '), 'en', 0.27),
+          name: 'sound.mp3'
+        } ]
+      })
+    } else {
+      message.channel.send({
+        files: [ {
+          attachment: await tts(args.filter(arg => !arg.startsWith('--')).join(' '), 'en', 1),
+          name: 'sound.mp3'
+        } ]
+      })
+    }
   } catch (e) {
     message.channel.send('Error!\nError data:\n' + e)
   }
