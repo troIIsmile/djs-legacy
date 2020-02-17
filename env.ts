c65af630f7bfd1c1a490e947581f235d9ffe0694
Jack <29169102+Jack5079@users.noreply.github.com>
2020-02-16 23:21:11 -0500

    Much better code

--- env.ts
+++ env.ts
@@ -3,13 +3,9 @@ import { readFileSync } from 'fs'
 interface Env {
   [ key: string ]: string
 }
-const env: Env = {}
 
-readFileSync('./.env', 'utf-8')
+const env: Env = Object.fromEntries(readFileSync('./.env', 'utf-8')
   .split('\n') // split the file into lines
   .map(line => line.split('=')) // split the lines into key:value pairs
-  .forEach(line => { // for each line
-    env[line[0]] = line[1] // env key = value
-  })
-
+)
 export default env
