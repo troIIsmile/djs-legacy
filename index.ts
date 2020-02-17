383a74e5eebefc5c86acba3b9754f95685a4870f
Jack <29169102+Jack5079@users.noreply.github.com>
2020-02-17 15:24:49 -0500

    The command folder refactor

--- index.ts
+++ index.ts
@@ -1,23 +1,30 @@
 import Bot from 'jackbot-discord'
 import { readdirSync } from 'fs'
-import env from './env'
-
-async function getCommand (file: string): Promise<[ string, Function ]> {
-  return [ file, (await import('./commands/' + file)).default ]
-}
+import { env } from './util'
 
 const bot = new Bot({}, {
   prefix: '-'
-});
-
-(async () => {
-  bot.add(Object.fromEntries( // Object.fromEntries does this: [['hello', 2]] -> {hello:2}
-    await Promise.all(
-      readdirSync('./commands/', 'utf-8') // get the file names of every command in the commands folder
-        .filter(filename => filename.endsWith('.js')) // only ones with `.js` at the end
-        .map(filename => filename.replace('.js', '')) // remove `.js` from those
-        .map(file => getCommand(file)))
-  ))
-})()
+})
 
 bot.login(env.TOKEN) // login using the token from .env
+
+interface Commands {
+  [ key: string ]: Function
+}
+
+async function getCommandsFromFolder (folder: string): Promise<Commands> {
+  async function getCommand (file: string): Promise<[ string, Function ]> {
+    return [ file, (await import(folder + file)).default ]
+  }
+
+  return Object.fromEntries( // Object.fromEntries does this: [['hello', 2]] -> {hello:2}
+    await Promise.all(
+      readdirSync(folder, 'utf-8') // get the file names of every command in the commands folder
+        .filter(filename => filename.endsWith('.js')) // only ones with `.js` at the end
+        .map(filename => filename.replace('.js', '')) // remove `.js` from those
+        .map(file => getCommand(file))
+    )
+  )
+}
+
+getCommandsFromFolder('./commands/').then(bot.add.bind(bot))
--- util.ts
+++ util.ts
@@ -1,5 +1,16 @@
+import { readFileSync } from 'fs'
+
 export function hasFlag (args: string[], flag: string): boolean {
   return args
     .filter(arg => arg.startsWith('--')) // only flags
     .includes('--' + flag) // does any of the args have the flag
 }
+
+interface Env {
+  [ key: string ]: string
+}
+
+export const env: Env = Object.fromEntries(readFileSync('./.env', 'utf-8')
+  .split('\n') // split the file into lines
+  .map(line => line.split('=')) // split the lines into key:value pairs
+)
