5b38690423f65396444fb24c4cdebc1bcf6bc64f
Jack <29169102+Jack5079@users.noreply.github.com>
2020-02-16 23:23:55 -0500

    use object.from entries

--- index.ts
+++ index.ts
@@ -3,19 +3,21 @@ import { readdirSync } from 'fs'
 import env from './env'
 
 async function getCommand (file: string): Promise<[ string, Function ]> {
-  return [file, (await import('./commands/' + file)).default]
+  return [ file, (await import('./commands/' + file)).default ]
 }
 
 const bot = new Bot({}, {
   prefix: '-'
-})
+});
 
-readdirSync('./commands/', 'utf-8') // get the file names of every command in the commands folder
-  .filter(filename => filename.endsWith('.js')) // only ones with `.js` at the end
-  .map(filename => filename.replace('.js', '')) // remove `.js` from those
-  .forEach(async file => {
-    bot.add(...await getCommand(file))
-    console.log('Loaded', file + '.js')
-  })
+(async () => {
+  bot.add(Object.fromEntries( // Object.fromEntries does this: [['hello', 2]] -> {hello:2}
+    await Promise.all(
+      readdirSync('./commands/', 'utf-8') // get the file names of every command in the commands folder
+        .filter(filename => filename.endsWith('.js')) // only ones with `.js` at the end
+        .map(filename => filename.replace('.js', '')) // remove `.js` from those
+        .map(file => getCommand(file)))
+  ))
+})()
 
 bot.login(env.TOKEN) // login using the token from .env
