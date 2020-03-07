0eb6a8866b08ada90437bfe4578cf64be9eef919
Jack <29169102+Jack5079@users.noreply.github.com>
2020-03-07 17:14:01 -0500

    finally add eval

---
+++ commands/eval.ts
@@ -0,0 +1,33 @@
+import { Bot, Message } from 'jackbot-discord'
+import { env } from '../util'
+
+async function clean (client: Bot, text: any): Promise<String> {
+  if (text && text.constructor.name == "Promise")
+    text = await text
+  if (typeof text !== "string")
+    text = require("util").inspect(text, { depth: 1 })
+
+  text = text
+    .replace(/`/g, "`" + String.fromCharCode(8203))
+    .replace(/@/g, "@" + String.fromCharCode(8203))
+    .replace(client.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0")
+
+  return text
+}
+
+async function run (message: Message, args: string[], client: Bot): Promise<void> {
+  if (message.author.id === env.OWNER) {
+    const code = args.join(" ")
+    try {
+      const evaled = eval(code)
+      const txt = await clean(client, evaled)
+      message.channel.send(`\`\`\`js\n${txt}\n\`\`\``)
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${await clean(client, err)}\n\`\`\``)
+    }
+  } else {
+    message.channel.send('You are not Jack.')
+  }
+}
+
+export default run
