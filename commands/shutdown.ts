f0f1a5e3fe83fdc6f042b2a153e722cf88cdb7e4
Jack <29169102+Jack5079@users.noreply.github.com>
2020-03-10 16:02:59 -0400

    how didn't i notice that

--- commands/shutdown.ts
+++ commands/shutdown.ts
@@ -2,8 +2,8 @@ import { Bot, Message } from 'jackbot-discord'
 import { env } from '../util'
 async function command (message: Message, _: string[], bot: Bot) {
   if (message.author.id === env.OWNER) {
+    message.channel.send('bye!')
     bot.destroy()
-    message.reply('Bot is shutting down.')
     process.exit(0)
   } else message.channel.send('you are not the bot owner')
 }
