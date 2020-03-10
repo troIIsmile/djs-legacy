15d1d6139edd9f1abb59edb95ca2405cb1060a34
Jack <29169102+Jack5079@users.noreply.github.com>
2020-03-09 20:14:10 -0400

    formatting

--- commands/eval.ts
+++ commands/eval.ts
@@ -3,17 +3,15 @@ import { env, clean } from '../util'
 
 async function run (message: Message, args: string[], client: Bot): Promise<void> {
   if (message.author.id === env.OWNER) {
-    const code = args.join(" ")
     try {
+      const code = args.join(" ")
       const evaled = eval(code)
       const txt = await clean(client, evaled)
       message.channel.send(`\`\`\`js\n${txt}\n\`\`\``)
     } catch (err) {
       message.channel.send(`\`ERROR\` \`\`\`xl\n${await clean(client, err)}\n\`\`\``)
     }
-  } else {
-    message.channel.send('You are not Jack.')
-  }
+  } else message.channel.send('You are not Jack.')
 }
 
 export default run
