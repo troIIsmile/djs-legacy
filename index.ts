a9010d0ce0de5aeafcebe2b8e9a0f20d41e411fe
Jack <29169102+Jack5079@users.noreply.github.com>
2020-03-11 18:53:24 -0400

    update to typescript 3.8.3

--- index.ts
+++ index.ts
@@ -13,16 +13,12 @@ if (!env.TOKEN) { // if there's no token
 } else bot.login(env.TOKEN) // login using the token from .env
 
 async function readCommandDir (folder: string): Promise<Commands> {
-  async function fileToCommand (file: string): Promise<[ string, Command ]> {
-    return [ file, (await import(folder + file)).default ]
-  }
-
   return Object.fromEntries( // Object.fromEntries does this: [ ['hello', 2] ] -> { hello: 2 }
     await Promise.all(
       (await fs.readdir(folder, 'utf-8')) // get the file names of every command in the commands folder
         .filter(filename => filename.endsWith('.js')) // only ones with `.js` at the end
         .map(filename => filename.replace('.js', '')) // remove `.js` from those
-        .map(fileToCommand) // convert filenames to commands
+        .map(async file => [ file, (await import(folder + file)).default ]) // convert filenames to commands
     )
   )
 }
