69ced844d5f97f1ab1de6b8a68cfc37cc27c06da
Glitch (nxtbot) <none>
2020-03-17 19:54:37 +0000

    🌋☄️ Updated with Glitch

--- commands/owo.ts
+++ commands/owo.ts
@@ -1,5 +1,5 @@
 
 import { Message } from 'jackbot-discord'
 const faces = [ 'OwO', 'UwU', '-w-' ]
-export const run = (message: Message, args: string[]) => message.channel.send(`\\*${args.join(' ')}\\* ${faces[ Math.floor(Math.random() * faces.length) ]}`)
+export const run = (message: Message, args: string[]) => message.channel.send(`*${args.join(' ')}* ${faces[ Math.floor(Math.random() * faces.length) ]}`)
 export const desc = 'Pwease use my Disoword bot UwU'
