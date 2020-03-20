2118df7221a9956342948fe842afaa2f819b2a86
Glitch (nxtbot) <none>
2020-03-20 00:38:08 +0000

    add timestamp

--- commands/xkcd.ts
+++ commands/xkcd.ts
@@ -7,13 +7,16 @@ export const run = async (message: Message, args: string[]) => {
   try {
     const url = args.length > 0 && args[0].match(/^\d+$/) ? `https://xkcd.com/${args[0]}/info.0.json` : "https://xkcd.com/info.0.json";
     const data = await fetch(url)
-    message.channel.send(new RichEmbed()
-      .setTitle(data.safe_title)
-      .setImage(data.img)
-      .setDescription(data.alt)
-      .setAuthor('XKCD', 'https://dogechain.info/favicon.png')
-      .setURL(`https://xkcd.com/${data.num}`)
-      .setColor(0x89c496))
+    message.channel.send(
+      new RichEmbed()
+        .setTitle(data.safe_title)
+        .setImage(data.img)
+        .setDescription(data.alt)
+        .setAuthor('XKCD', 'https://dogechain.info/favicon.png')
+        .setURL(`https://xkcd.com/${data.num}`)
+        .setColor(0x89c496)
+        .setTimestamp(new Date(`${data.year}-${data.month}-${data.day}`))
+    )
   } catch (error) {
     message.channel.send(new RichEmbed()
       .setAuthor('XKCD', 'https://dogechain.info/favicon.png')
