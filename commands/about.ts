588925d2312572e4ca03d9d833395c35f1d10a3f
Glitch (nxtbot) <none>
2020-03-22 21:20:31 +0000

    add credits in about

--- commands/about.ts
+++ commands/about.ts
@@ -15,6 +15,12 @@ export const run = (message: Message, _: string[], bot: Bot) => {
   const formatted = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0')
 
   message.channel.send(`
+  NXTBOT by Jack#9701
+  
+  "Playing" statuses stolen from esmBot
+  The text -> fullwidth converter code stolen from esmBot
+  The concept for the qrcreate and xkcd commands stolen from esmBot
+  I got the idea for fake* commands from the whiskers bot (whiskers only has fakeperson)
   // How long the bot has been on for
   Uptime: ${formatted}
   Server count: ${bot.guilds.cache.size}
