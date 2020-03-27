fb44972ee0d73ff114d0e1bb0dd05d7de0f6a6c4
Glitch (nxtbot) <none>
2020-03-27 20:26:46 +0000

    now it gets owner info based off of env and stuff

--- commands/about.ts
+++ commands/about.ts
@@ -14,19 +14,29 @@ export const run = (message: Message, _: string[], bot: Bot) => {
 
   const formatted = `${hours} hour(s), ${minutes} minute(s), and ${seconds} second(s).`
   
+  delete require.cache[require.resolve('../package.json')] // Always get the latest package.json
+  
+  const owner = bot.users.cache.get(process.env.OWNER as string)
+  
+  if (!(bot.user && owner)) return 'oops the owner or the bot user does not exist some how'
+  
   return {
     embed: {
       author: {
-        name: 'NXTBOT Info',
+        name: `${bot.user.tag.split('#')[0]} Info`, // I'm sorry this is the only way I could figure out how
         iconURL: bot.user?.displayAvatarURL()
       },
       color: 0x454545,
-      title: 'Developed by *Jack#9701*',
+      footer: {
+        text: `Developed by ${owner.tag}`,
+        iconURL: owner.displayAvatarURL()
+      },
       fields: [{
         name: '✏ Credits',
         value: `
         Some snippets of code from Guidebot by eslachance and esmBot by **Essem#9261**
-        [Some "Playing" messages from esmBot](https://github.com/TheEssem/esmBot/blob/master/messages.json)`
+        [Some "Playing" messages from esmBot](https://github.com/TheEssem/esmBot/blob/master/messages.json)`,
+        inline: true
       }, {
         name: '💬 Server Count',
         value: bot.guilds.cache.size,
