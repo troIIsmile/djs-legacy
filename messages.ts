013efdb9ad226812492643ed3cb5044c8a813aea
Glitch (nxtbot) <none>
2020-03-28 01:33:49 +0000

    improve messages.ts

--- messages.ts
+++ messages.ts
@@ -4,6 +4,7 @@ interface Messages {
   [key: string]: string[]
 }
 
+
 const messages: Messages = {
   Browsers: [
     'Mozilla Firefox',
@@ -15,28 +16,35 @@ const messages: Messages = {
     'Microsoft Internet Explorer',
     'Netscape Navigator'
   ],
-  'Operating Systems': [
-    'Ubuntu',
-    'Linux', // Not sure about this one
-    'SteamOS',
-    // https://en.wikipedia.org/wiki/List_of_Microsoft_Windows_versions
-    'Windows 10',
-    'Windows 8.1',
-    'Windows 8',
-    'Windows 7',
-    'Windows Vista',
-    'Windows XP',
-    'Windows Me',
-    'Windows 2000',
-    'Windows 98',
-    'Windows 95',
-    'Windows 3.1',
-    'Windows 1.0'
-  ],
+  'Operating Systems': {
+    Linux: [
+      'Ubuntu',
+      'KDE neon', // kde users when they see a lawn gnome in their neighbor's yard
+      'Kubuntu',
+      'Arch Linux',
+      'SteamOS',
+      'Hannah Montana Linux' // The one true Linux distro.
+    ],
+    Windows: [
+      'Windows 10',
+      'Windows 7',
+      'Windows Vista',
+      'Windows XP',
+      'Windows 95',
+      'Windows 3.1'
+    ]
+  },
   Apps: [ // Apps are programs on mobile
     'TikTok',
-    'iPod Music',
-    'Skype' // I mean it's on mobile now
+    'iPod Music'
+  ],
+  Programs: [ // Programs are apps on PC
+    'MS Paint',
+    'Skype', // Communication tool for free calls and chat
+    'Blender', // The free and open source 3D creation suite.
+    'Visual Studio Code', // Visual Studio without the Visual
+    'Atom', // A hackable text editor for the 21st Century
+    'Shotcut' // Free, open source, and cross-platform video editor.
   ],
   Nintendo: [
     'Super Mario Run',
@@ -69,17 +77,45 @@ const messages: Messages = {
     'single quote no semicolons two spaces',
     'Submit playing lines on github: Jack5079/nxtbot'
   ],
+  Songs: Object.entries({
+    'Scatman John': [
+      "Scatman's World"
+    ],
+    'Alex Arcoleo': [
+      'Bloom 7'
+    ],
+    'Neil Cicierega': [
+      'Wow Wow',
+      'The Starting Line'
+    ],
+    TOPAZ: [ // topazzz.bandcamp.com to be exact
+      'Half Awake, Pt. 2',
+      'Half Awake, Pt. 1'
+    ],
+    'Fatty Spins': [
+      "Doin' Your Mom", // you know we straight
+    ],
+    'FAT DAMON': [
+      'Conspiracy Theory Guy'
+    ],
+    'Your Favorite Martian': [
+      "Grandma's Got A Facebook"
+    ]
+  }).map(name => name[1].map(title=>`${name[0]} - ${title}`)).flat(), // Terrible code to turn it into song names
   Games: [ // Finally, the actual games lmao
     'Pac-Man Championship Edition DX+',
     'Pac-Man Championship Edition 2',
     'Club Penguin',
+    'Club Penguin Island',
     'Hong Kong 97',
     "Sonic's Schoolhouse",
     'Desert Bus',
     'Shrek Extra Large', // Somehow a real game.
-    'Roblox', // I know this is a game engine but
+    'Super Bernie World', // Yes this is too
+    'Roblox', // I know this is a game engine
     'Minecraft',
     'Skyrim',
+    'SuperTuxKart', // Force restarts my PC when I press Enter or Space while paused.
     'Sonic 06',
     'Metal Gear Solid 4',
     'Action 52',
@@ -96,23 +132,23 @@ const messages: Messages = {
     'Mega Man Legends 3',
     'BLJ Simulator',
   ],
-  Songs: [
-    "Scatman's World"
-  ],
   Puns: [
     'with your sanity',
     'with yo mama',
     'with a broken god',
     'games with the mortals',
-    'with the Infinity Gauntlet'
+    'with the Infinity Gauntlet',
+    'with your Discord server',
+    'with a stone, Luigi.',
+    'the Cat Piano',
   ],
   Websites: [
-    'Twitter',
-    'Reddit',
-    'YouTube',
+    'Twitter', // "It's what's happening."
+    'Reddit', // will keep you updated
+    'YouTube', // "Broadcast Yourself"
     'MDN Web Docs' // the current name
   ],
-  Random: [
+  Random: [ // All of these are from esmBot.
     'h',
     'a game',
     'anime',
@@ -122,9 +158,7 @@ const messages: Messages = {
     'gniyalP',
     'HaaH WaaW',
     'dQw4w9WgXcQ',
-    'MS Paint',
     'the funny memes epic',
-    'the Cat Piano',
     'Bottom Text',
     'lol 7',
     'Family Guy',
@@ -132,18 +166,24 @@ const messages: Messages = {
     'Rofa Cat',
     'jeff',
     'woo yeah',
-    'with Edgar',
     'joe mama',
-    'Liquid Sun',
-    'with your Discord server',
-    'with a stone, Luigi.',
-    'doin your mom doin doin your mo',
     '#BringBackNationalSex',
     'foobar2000',
     'with GIFs'
   ]
 }
 
-const all = Object.values(messages).flat()
+// Thanks to https://stackoverflow.com/a/49042916 for the flatten function
+const flatten = (obj, path = '') => {        
+    if (!(obj instanceof Object)) return {[path.replace(/\.$/g, '')]:obj};
+
+    return Object.keys(obj).reduce((output, key) => {
+        return obj instanceof Array ? 
+             {...output, ...flatten(obj[key], path +  '[' + key + '].')}:
+             {...output, ...flatten(obj[key], path + key + '.')};
+    }, {});
+}
+
+const all = Object.values(flatten(messages)).flat()
 
 export { messages, all }
