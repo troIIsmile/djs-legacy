0d83ffdf476fe62a1a125ba712c709b287ebe169
Jack <29169102+Jack5079@users.noreply.github.com>
2020-04-29 19:35:02 -0400

    Better looking error

--- commands/mcsteal.ts
+++ commands/mcsteal.ts
@@ -2,8 +2,8 @@ import { Message } from 'discord.js'
 import fetch from 'node-fetch'
 export async function run (_message: Message, args: string[]) {
   const res = await fetch('https://minecraftskinstealer.com/api/v1/skin/render/skin/' + encodeURI(args.join(' ')))
-  if (res.ok) {
-    return {
+  return res.ok
+    ? { // If it worked
       files: [
         {
           attachment: await res.buffer(),
@@ -11,13 +11,12 @@ export async function run (_message: Message, args: string[]) {
         }
       ]
     }
-  } else {
-    return {
+    : { // If it failed
       embed: {
-        title: res.status === 404 ? 'Player not found!' : `${res.status}! ${res.statusText}`,
-        description: 'OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo!'
+        title: res.status === 404 ? 'Player not found!' : `${res.status}: ${res.statusText}`,
+        description: 'OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo!',
+        color: 0xFF0000
       }
     }
-  }
 }
 export const desc = 'mcsteal - Get the skin of a Minecraft: Java Edition player. Syntax: mcsteal <username>'
