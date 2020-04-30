766638f0e2c9fc52d2843edffe1d990dfd843910
Jack <29169102+Jack5079@users.noreply.github.com>
2020-04-30 13:25:32 -0400

    simplify ok

--- commands/ok.ts
+++ commands/ok.ts
@@ -1,5 +1,3 @@
-import { Message } from 'discord.js'
-const command = (message: Message) => 'retard'
 export const desc =
   'Was made in 45 seconds. Only replies with "retard". That\'s it.'
-export const run = command
+export const run = () => 'retard'
