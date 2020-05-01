f461d37aa9db577b602dc2e6223c48acfa377e6a
Jack <29169102+Jack5079@users.noreply.github.com>
2020-05-01 09:06:02 -0400

    Revert "same with this"
    This reverts commit aa71c7a70a461ddb9829df9ba8337244218810c2.

--- commands/eval.ts
+++ commands/eval.ts
@@ -3,19 +3,19 @@ import clean from '../utils/clean'
 import { hasPerm } from '../utils/permissions'
 import { Bot } from '../utils/types'
 
-export function run (
+export async function run (
   message: Message,
   args: string[],
   client: Bot
-): string {
+): Promise<string> {
   if (hasPerm(message)) {
     try {
       const code = args.join(' ')
       const evaled = eval(code)
-      const txt = clean(client, evaled)
+      const txt = await clean(client, evaled)
       return `\`\`\`js\n${txt}\n\`\`\``
     } catch (err) {
-      return `\`ERROR\` \`\`\`xl\n${clean(client, err)}\n\`\`\``
+      return `\`ERROR\` \`\`\`xl\n${await clean(client, err)}\n\`\`\``
     }
   } else return 'You are not the bot owner.'
 }
