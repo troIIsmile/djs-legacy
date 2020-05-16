a6811a1311182c05ed393f06d2f55c3281b92cec
Jack <29169102+Jack5079@users.noreply.github.com>
2020-05-16 19:47:47 -0400

    Fix avatar command

--- commands/avatar.ts
+++ commands/avatar.ts
@@ -1,23 +1,20 @@
 import { Message } from 'discord.js'
 
-export const run = (message: Message) => {
-  if (!message.mentions.users.size) {
-    return {
-      files: [
-        {
-          attachment: message.author.displayAvatarURL,
-          name: 'avatar.png'
-        }
-      ]
-    }
-  } else
-    return {
-      files: [
-        {
-          attachment: message.mentions.users.first()?.displayAvatarURL(),
-          name: 'avatar.png'
-        }
-      ]
-    }
-}
+export const run = (message: Message) => message.mentions.users.size
+  ? {
+    files: [
+      {
+        attachment: message.mentions.users.first()?.displayAvatarURL(),
+        name: 'avatar.png'
+      }
+    ]
+  }
+  : {
+    files: [
+      {
+        attachment: message.author.displayAvatarURL(),
+        name: 'avatar.png'
+      }
+    ]
+  }
 export const desc = 'Get the avatar of a user.'
