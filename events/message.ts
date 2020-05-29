f5ce5a716584b3b6a72db4715513a4535b004d9e
Jack <29169102+Jack5079@users.noreply.github.com>
2020-05-29 07:01:05 -0400

    Move command handler to message.ts

---
+++ events/message.ts
@@ -0,0 +1,50 @@
+import { Message } from 'discord.js'
+import { Bot } from '../utils/types'
+import { prefixes } from '../util'
+
+// NXTBOT Command Handler (This used to be jackbot-discord!)
+export default async (message: Message, bot: Bot) => {
+  // When a message is sent
+  if (!message.author?.bot) {
+    // no bots allowed
+    const prefix: string = prefixes[message.guild?.id || ''] || '-'
+    const content = message.content || ''
+    const name = [...bot.commands.keys(), ...bot.aliases.keys()].find(
+      cmdname =>
+        content.startsWith(`${prefix}${cmdname} `) || // matches any command with a space after
+        content === `${prefix}${cmdname}` // matches any command without arguments
+    )
+    // Run the command!
+    if (name) {
+      const command = bot.commands.get(name)?.run // The command if it found it
+        || bot.commands.get(bot.aliases.get(name) || '')?.run // Aliases
+        || (() => { }) // Do nothing otherwise
+
+      try {
+        const output = await command(
+          message as Message, // the message
+          // The arguments
+          content
+            .substring(prefix.length + 1 + name.length) // only the part after the command
+            .split(' '), // split with spaces
+          bot // The bot
+        )
+
+        if (output) message.channel?.send(output)
+      } catch (err) {
+        message.channel?.send({
+          embed: {
+            author: {
+              name: `${bot.user?.username} ran into an error while running your command!`,
+              iconURL: bot.user?.avatarURL()
+            },
+            title: err.toString(),
+            footer: {
+              text: 'Report this bug @ ' + require('./package.json').bugs
+            }
+          }
+        })
+      }
+    }
+  }
+}
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Client, Message, Collection } from 'discord.js'
+import { Client, Collection } from 'discord.js'
 import { Bot, CommandObj } from './utils/types'
 import {
   existsSync as exists,
@@ -7,7 +7,6 @@ import {
 } from 'fs'
 import { IncomingMessage, ServerResponse, createServer } from 'http'
 import { rreaddir } from './utils/rreaddir'
-import { prefixes } from './util'
 
 // We need to get data from the .env file because OWNER and TOKEN are in there ( unless the user somehow does stuff like `'blahblahblah' > Env:/TOKEN`)
 if (exists('./.env')) {
@@ -68,53 +67,6 @@ async function loadCommands () {
 }
 
 loadCommands()
-
-// Remember jackbot-discord? This is it now.
-bot.on('message', async message => {
-  // When a message is sent
-  if (!message.author?.bot) {
-    // no bots allowed
-    const prefix: string = prefixes[message.guild?.id || ''] || '-'
-    const content = message.content || ''
-    const name = [...bot.commands.keys(), ...bot.aliases.keys()].find(
-      cmdname =>
-        content.startsWith(`${prefix}${cmdname} `) || // matches any command with a space after
-        content === `${prefix}${cmdname}` // matches any command without arguments
-    )
-    // Run the command!
-    if (name) {
-      const command = bot.commands.get(name)?.run // The command if it found it
-        || bot.commands.get(bot.aliases.get(name) || '')?.run // Aliases
-        || (() => { }) // Do nothing otherwise
-
-      try {
-        const output = await command(
-          message as Message, // the message
-          // The arguments
-          content
-            .substring(prefix.length + 1 + name.length) // only the part after the command
-            .split(' '), // split with spaces
-          bot // The bot
-        )
-
-        if (output) message.channel?.send(output)
-      } catch (err) {
-        message.channel?.send({
-          embed: {
-            author: {
-              name: `${bot.user?.username} ran into an error while running your command!`,
-              iconURL: bot.user?.avatarURL()
-            },
-            title: err.toString(),
-            footer: {
-              text: 'Report this bug @ ' + require('./package.json').bugs
-            }
-          }
-        })
-      }
-    }
-  }
-})
 
 // Load in events
 readdirSync('./events/')
