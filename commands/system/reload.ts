9dc54d1219eeb3a38e958efee14202cec1e72937
Jack <29169102+Jack5079@users.noreply.github.com>
2020-05-29 07:13:53 -0400

    -reload now works with aliases!

--- commands/system/reload.ts
+++ commands/system/reload.ts
@@ -4,18 +4,19 @@ import { hasPerm } from '../../utils/permissions'
 
 export async function run (message: Message, args: string[], bot: Bot) {
   if (hasPerm(message)) {
-    if (bot.commands.get(args.join(' '))?.path) {
+    const cmdname = (bot.commands.get(args.join(' ')) ? args.join(' ') : (bot.commands.get(bot.aliases.get(args.join(' ')) || '') ? bot.aliases.get(args.join(' ')) : '')) || ''
+    if (bot.commands.get(cmdname)?.path) {
       // Remove old aliases
-      if (bot.commands.get(args.join(' '))?.aliases) {
-        bot.commands.get(args.join(' '))?.aliases?.forEach(Map.prototype.delete.bind(bot.aliases))
+      if (bot.commands.get(cmdname)?.aliases) {
+        bot.commands.get(cmdname)?.aliases?.forEach(Map.prototype.delete.bind(bot.aliases))
       }
-      const path = bot.commands.get(args.join(' '))?.path! // Jesus fucking christ TypeScript I just need the path
+      const path = bot.commands.get(cmdname)?.path! // Jesus fucking christ TypeScript I just need the path
       delete require.cache[path] // Remove require's cache so we can import the new one
-      bot.commands.set(args.join(' '), { ...(await import(path)), path }) // Loads in a newly imported command along with the path
+      bot.commands.set(cmdname, { ...(await import(path)), path }) // Loads in a newly imported command along with the path
       // If there are aliases in the reloaded command, add them
-      if (bot.commands.get(args.join(' '))?.aliases) {
-        bot.commands.get(args.join(' '))?.aliases?.forEach(alias => {
-          bot.aliases.set(alias, args.join(' '))
+      if (bot.commands.get(cmdname)?.aliases) {
+        bot.commands.get(cmdname)?.aliases?.forEach(alias => {
+          bot.aliases.set(alias, cmdname)
         })
       }
       return {
@@ -25,11 +26,11 @@ export async function run (message: Message, args: string[], bot: Bot) {
             iconURL: bot.user?.displayAvatarURL()
           },
           color: 0x00FF00, // Checkmark green
-          title: args.join(' '),
+          title: cmdname,
           description: (await import(path)).desc
         }
       }
-    } else return '❌ That command does not exist, was added with the add command, or is an alias.'
+    } else return '❌ That command does not exist, or was added with the add command.'
   } else return '❌ This command is for the bot owner only.'
 }
 export const desc = 'Reloads a command.'
