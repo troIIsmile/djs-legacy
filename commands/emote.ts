5e52006705fb6c3c717750fdec0a9dd26ce17301
Jack <29169102+Jack5079@users.noreply.github.com>
2020-05-29 15:58:47 -0400

    fix emote

--- commands/emote.ts
+++ commands/emote.ts
@@ -1,10 +1,12 @@
 import { Message } from 'discord.js'
 import { Bot } from '../utils/types'
 import random from '../utils/random'
-export const run = (_message: Message, _args: string[], client: Bot) => random(
-  client.guilds.cache
-    .map(guild => guild.emojis.cache.array())
-    .flat()
-    .filter(Boolean).map(emoji => emoji.toString())
-) || 'The bot somehow has no emoji. What the?'
+export const run = function (this: Bot, _message: Message, _args: string[]) {
+  return random(
+    this.guilds.cache
+      .map(guild => guild.emojis.cache.array())
+      .flat()
+      .filter(Boolean).map(emoji => emoji.toString())
+  ) || 'The bot somehow has no emoji. What the?'
+}
 export const desc = 'Random emote from the servers this bot is in'
