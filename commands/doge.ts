11743a10a7a5e64723d76ff866c17cf1d92551e2
Jack <29169102+Jack5079@users.noreply.github.com>
2020-05-31 21:26:06 -0400

    Remove data.json

--- commands/doge.ts
+++ commands/doge.ts
@@ -1,23 +1,10 @@
 // TODO: Add VC like the old jackbot
 import { Message } from 'discord.js'
-import { persist } from '../util'
 import ytdl from 'ytdl-core'
-
-// nth is from https://stackoverflow.com/questions/13627308/add-st-nd-rd-and-th-ordinal-suffix-to-a-number
-function nth (i: number): string {
-  const j = i % 10
-  const k = i % 100
-  if (j == 1 && k != 11) return i + 'st'
-  if (j == 2 && k != 12) return i + 'nd'
-  if (j == 3 && k != 13) return i + 'rd'
-  return i + 'th'
-}
-
 export async function run (message: Message) {
-  if (typeof persist.dogeCount !== 'number') persist.dogeCount = 0
   message.channel.send({
     file: 'http://assets.stickpng.com/thumbs/5845e755fb0b0755fa99d7f3.png',
-    content: `le doge has arrived for the ${nth(++persist.dogeCount)} time!`
+    content: `le doge has arrived`
   })
   if (message.member?.voice.channel) {
     const channel = message.member.voice.channel
--- util.ts
+++ util.ts
@@ -5,25 +5,8 @@ export function hasFlag (args: string[], flag: string): boolean {
     .includes('--' + flag) // does any of the args have the flag
 }
 
-if (!exists('./data.json')) write('./data.json', '{}')
 if (!exists('./prefixes.json')) write('./prefixes.json', '{}')
 
-type JSONTypes = string | number | boolean | null | PersistStorage | JSONTypes[]
-
-interface PersistStorage {
-  [key: string]: JSONTypes
-}
-export const persist: PersistStorage = new Proxy({}, {
-  get (_, name) {
-    return require('./data.json')[name]
-  },
-  set (_, prop: string, value: any): boolean {
-    let data = require('./data.json')
-    data[prop] = value
-    write('./data.json', JSON.stringify(data, null, 2))
-    return true
-  }
-})
 
 interface Prefixes {
   [key: string]: string
