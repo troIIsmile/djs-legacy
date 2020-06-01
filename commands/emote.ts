cfe896283d9c3892fbf97760178eadbaf418fe3d
Jack <29169102+Jack5079@users.noreply.github.com>
2020-06-01 15:55:01 -0400

    remove args

--- commands/emote.ts
+++ commands/emote.ts
@@ -1,7 +1,6 @@
-import { Message } from 'discord.js'
 import { Bot } from '../utils/types'
 import random from '../utils/random'
-export const run = function (this: Bot, _message: Message, _args: string[]) {
+export function run (this: Bot) {
   return random(
     this.guilds.cache
       .map(guild => guild.emojis.cache.array())
