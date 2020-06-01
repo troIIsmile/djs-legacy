42ab69d782dfb5542fa076cb87aec81304896023
Jack <29169102+Jack5079@users.noreply.github.com>
2020-06-01 12:02:31 -0400

    add typings

--- commands/internet/image/linusrant.ts
+++ commands/internet/image/linusrant.ts
@@ -13,7 +13,7 @@ function escapeMarkdown (text: string) {
 
 const rants: Promise<Rant[]> = fetch('https://raw.githubusercontent.com/corollari/linusrants/master/data.json').then(res => res.json())
 export async function run (): Promise<MessageOptions> {
-  const { text, hate } = random(await rants)
+  const { text, hate }: Rant = random(await rants)
   return {
     embed: {
       author: {
