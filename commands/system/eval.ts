7d6899158e30709df3b57766bf9700f576a5dca7
Jack <29169102+Jack5079@users.noreply.github.com>
2020-05-31 22:06:36 -0400

    add hastebin support

--- commands/system/eval.ts
+++ commands/system/eval.ts
@@ -1,22 +1,38 @@
-import { Message } from 'discord.js'
+import { Message, MessageOptions } from 'discord.js'
 import clean from '../../utils/clean'
 import { hasPerm } from '../../utils/permissions'
 import { Bot } from '../../utils/types'
+import fetch from 'node-fetch'
 
 export async function run (
   this: Bot,
   message: Message,
   args: string[]
-): Promise<string> {
+): Promise<MessageOptions> {
   if (hasPerm(message)) {
     try {
       const code = args.join(' ')
       const evaled = eval(code)
       const txt = await clean(this, evaled)
-      return `\`\`\`js\n${txt}\n\`\`\``
+      const msg = `\`\`\`js\n${txt}\n\`\`\``
+      if (msg.length <= 2000) return { content: msg }
+
+      const { key } = await fetch(`https://hastebin.com/documents`, {
+        method: "POST",
+        body: txt,
+        headers: { "Content-Type": "text/plain" }
+      }).then(res => res.json())
+      
+      return {
+        embed: {
+          title: 'Uploaded!',
+          url: `https://hastebin.com/${key}`,
+          color: 'GREEN'
+        }
+      }
     } catch (err) {
-      return `\`ERROR\` \`\`\`xl\n${await clean(this, err)}\n\`\`\``
+      return { content: `\`ERROR\` \`\`\`xl\n${await clean(this, err)}\n\`\`\`` }
     }
-  } else return 'You are not the bot owner.'
+  } else return { content: 'You are not the bot owner.' }
 }
 export const desc = 'give it code and it runs it'
