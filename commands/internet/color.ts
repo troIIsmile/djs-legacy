2e274c221e93e3bc6b210edaec0bbcbd370b2ddc
Jack <29169102+Jack5079@users.noreply.github.com>
2020-06-10 21:29:07 -0400

    add color command

---
+++ commands/internet/color.ts
@@ -0,0 +1,25 @@
+import { Message, MessageOptions } from 'discord.js'
+import fetch from 'node-fetch'
+export async function run (message: Message, args: string[]): Promise<MessageOptions> {
+  const clr = args.join('')
+  const res = await fetch('https://colornames.org/search/json/?hex=' + encodeURIComponent(clr))
+  if (res.ok) {
+    const { name } = await res.json()
+    return {
+      embed: {
+        title: name || 'Name not found!',
+        color: clr,
+        url: 'https://colornames.org/color/' + clr
+      }
+    }
+  } else {
+    return {
+      embed: {
+        color: 'RED',
+        title: 'That isn\'t a hex color!'
+      }
+    }
+  }
+}
+export const desc = 'Get the name of a color. '
+export const aliases = []
