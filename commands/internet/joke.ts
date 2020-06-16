a7cfc53f25dfff65d3292658617ac5fc7760ab59
Jack <29169102+Jack5079@users.noreply.github.com>
2020-06-15 20:33:17 -0400

    comedy

---
+++ commands/internet/joke.ts
@@ -0,0 +1,23 @@
+import fetch from 'node-fetch'
+import { MessageOptions } from 'discord.js'
+
+interface Joke {
+  setup: string
+  punchline: string
+  id: number
+}
+export async function run (): Promise<MessageOptions> {
+  const [{ setup, punchline }]: Joke[] = await fetch('https://official-joke-api.appspot.com/jokes/programming/random').then(res => res.json()) 
+  return {
+    embed: {
+      title: setup,
+      description: punchline,
+      author: {
+        name: 'Official Joke API',
+        url: 'https://github.com/15Dkatz/official_joke_api'
+      }
+    }
+  }
+}
+export const desc = 'is not funny'
+export const aliases = []
