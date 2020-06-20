57e4d6b5b0182698673e3466022ff502012aff88
Jack <29169102+Jack5079@users.noreply.github.com>
2020-06-20 14:27:12 -0400

    Increase size

--- commands/internet/image/adorable.ts
+++ commands/internet/image/adorable.ts
@@ -1,7 +1,7 @@
 import { Message, MessageOptions } from 'discord.js'
 export async function run (message: Message): Promise<MessageOptions> {
   return {
-    files: [`https://api.adorable.io/avatars/206/${message.author.username}.png`]
+    files: [`https://api.adorable.io/avatars/2048/${message.author.username}.png`]
   }
 }
 export const desc = 'Get an adorable avatar'
