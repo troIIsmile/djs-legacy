8d09070e3dd52c0411bdd13882e74c0afa31f8c0
Jack <29169102+Jack5079@users.noreply.github.com>
2020-06-20 14:22:55 -0400

    add author

--- commands/internet/image/8ball.ts
+++ commands/internet/image/8ball.ts
@@ -9,6 +9,11 @@ export async function run (): Promise<MessageOptions> {
   const { answer, image: url }: APIResult = await (await fetch('https://yesno.wtf/api')).json()
   return {
     embed: {
+      author: {
+        name: 'Yes Or No?',
+        url: 'https://yesno.wtf/',
+        iconURL: 'https://yesno.wtf/assets/favicons/favicon-32x32-32e4b13414766709719da720e81cf1b2.png'
+      },
       title: answer,
       image: {
         url
