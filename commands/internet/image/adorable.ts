f3acb5e9d5c31274850edcaa4479514f1e8dff1e
Jack <29169102+Jack5079@users.noreply.github.com>
2020-06-20 14:26:39 -0400

    wait i can just do this

--- commands/internet/image/adorable.ts
+++ commands/internet/image/adorable.ts
@@ -1,9 +1,7 @@
 import { Message, MessageOptions } from 'discord.js'
 export async function run (message: Message): Promise<MessageOptions> {
   return {
-    files: [{
-      attachment: `https://api.adorable.io/avatars/206/${message.author.username}.png`
-    }]
+    files: [`https://api.adorable.io/avatars/206/${message.author.username}.png`]
   }
 }
 export const desc = 'A new command'
