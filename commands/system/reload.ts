d448743e9d0188056cfdff77a5b40bca0849c11d
Jack <29169102+Jack5079@users.noreply.github.com>
2020-07-24 20:52:23 -0400

    idk

--- commands/system/reload.ts
+++ commands/system/reload.ts
@@ -7,14 +7,17 @@ export async function run (this: Bot, message: Message, args: string[],): Promis
   if (hasPerm(message)) {
     const cmdname = (this.commands.get(args.join(' ')) ? args.join(' ') : (this.commands.get(this.aliases.get(args.join(' ')) || '') ? this.aliases.get(args.join(' ')) : '')) || ''
     if (this.commands.get(cmdname)?.path) {
-      // Remove old aliases
+      // Remove the command's aliases
       if (this.commands.get(cmdname)?.aliases) {
         this.commands.get(cmdname)?.aliases?.forEach(Map.prototype.delete.bind(this.aliases))
       }
+
+      // Actually reload the command
       const path = this.commands.get(cmdname)?.path! // Jesus fucking christ TypeScript I just need the path
       delete require.cache[path] // Remove require's cache so we can import the new one
-      this.commands.set(cmdname, { ...(await import(path)), path }) // Loads in a newly imported command along with the path
-      // If there are aliases in the reloaded command, add them
+      this.commands.set(cmdname, { ...(await import(path)), path }); // Loads in a newly imported command along with the path
+      
+      // Add the aliases back
       if (this.commands.get(cmdname)?.aliases) {
         this.commands.get(cmdname)?.aliases?.forEach(alias => {
           this.aliases.set(alias, cmdname)
