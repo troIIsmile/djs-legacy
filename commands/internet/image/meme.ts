152aa2ba8752ff375ffda41a2ea420ebc9d2eaa3
Jack <29169102+Jack5079@users.noreply.github.com>
2020-09-18 22:45:09 +0000

    RANDOM TEMPLATE

--- commands/internet/image/meme.ts
+++ commands/internet/image/meme.ts
@@ -1,32 +1,51 @@
 import { Message, MessageOptions } from 'discord.js';
 import fetch from "node-fetch";
+import random from "../../../utils/random";
 import { Bot } from '../../../utils/types';
-
+type imgflip<memedata> = {
+  success: true,
+  data: memedata;
+} | {
+  success: false;
+  error_message: string;
+};
 export async function run (this: Bot, message: Message, args: string[]): Promise<string | MessageOptions> {
-  const [text0 = '', text1 = ''] = args.join(' ').split('|');
-  const rawResponse = await fetch('https://api.imgflip.com/caption_image?' + new URLSearchParams({
-    username: process.env.IMGFLIP_USERNAME || '', password: process.env.IMGFLIP_PASSWORD || '', text0, text1, template_id: "61579"
-  }).toString(), {
-    method: 'POST',
-  });
-  const content: {
-    success: true,
-    data: {
+  // getting the template
+
+  const memes: imgflip<{
+    memes: {
+      id: string;
+      name: string;
+      url: string;
+    }[];
+  }> = await fetch('https://api.imgflip.com/get_memes').then(res => res.json());
+  if (memes.success) {
+    // posting the meme
+    const [text0 = '', text1 = ''] = args.join(' ').split('|');
+    const content: imgflip<{
       url: string;
       page_url: string;
-    };
-  } | {
-    success: false;
-    error_message: string;
-  } = await rawResponse.json();
+    }> = await fetch('https://api.imgflip.com/caption_image?' + new URLSearchParams({
+      username: process.env.IMGFLIP_USERNAME || '', password: process.env.IMGFLIP_PASSWORD || '', text0, text1, template_id: random(memes.data.memes).id
+    }).toString(), {
+      method: 'POST',
+    }).then(res => res.json());
 
-  if (content.success) {
-    return content.data.page_url;
+    if (content.success) {
+      return content.data.page_url;
+    } else {
+      return {
+        embed: {
+          title: 'Imgflip error!',
+          description: content.error_message
+        }
+      };
+    }
   } else {
     return {
       embed: {
         title: 'Imgflip error!',
-        description: content.error_message
+        description: memes.error_message
       }
     };
   }
