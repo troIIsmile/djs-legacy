e64a8515f9efa5af17c6fc6889ccb35f81fb368f
Jack <29169102+Jack5079@users.noreply.github.com>
2020-12-13 09:45:15 -0500

    a

--- init.ts
+++ init.ts
@@ -1,9 +1,3 @@
-/**
- * @file Init
- * This file logs into Discord. It also loads events, the .env file, and the Web server (if it is required)
- * Fun fact: This used to be 14 lines! {@see https://github.com/troIIsmile/node/blob/bfdfd1c6aa5b4e67cc08192f59ac55ac92d40663/index.ts}
- * @author Jack <hello@5079.ml> (https://5079.ml)
- */
 import { Client, Collection, Intents } from 'discord.js'
 import { Bot } from './utils/types'
 import {
@@ -17,9 +11,8 @@ globalThis.Array.prototype.random = function () {
   return this[Math.floor(Math.random() * this.length)]
 }
 
-// We need to get data from the .env file because OWNER and TOKEN are in there ( unless the user somehow does stuff like `'blahblahblah' > Env:/TOKEN`)
+// dotenv support
 if (exists('./.env')) {
-  // Before anything uses it, we must load the .env file (provided it exists, of course)
   Object.assign(process.env,
     Object.fromEntries(
       // Overwrite the env with the .env file
