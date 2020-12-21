1278a2f3a510e112fc0c152519cc737a472109e5
Jack <29169102+Jack5079@users.noreply.github.com>
2020-12-21 10:16:44 -0500

    a

--- commands/fortnite.ts
+++ commands/fortnite.ts
@@ -1,11 +1,9 @@
 import { Message } from 'discord.js'
-export const run = (message: Message) => {
-  if (message.guild) {
-    return `
-You played Fortnite with ${message.guild.members.cache.random()}
-Your ${Math.random() >= 0.5 ? 'Battle Pass' : 'Free Pass'} tier is ${Math.floor(
-      Math.random() * 100
-    )}`
-  } else return 'You must be in a server to use this command!'
-}
+export const run = (message: Message) => (
+  message.guild ?
+    `
+    You played Fortnite with ${message.guild.members.cache.random()}
+    Your ${Math.random() >= 0.5 ? 'Battle Pass' : 'Free Pass'} tier is ${Math.floor(Math.random() * 100)}`
+    : 'You must be in a server to use this command!'
+)
 export const help = '@someone but funnier'
