1d547736d9954e749adef5024c430a6b5111fd72
Jack <29169102+Jack5079@users.noreply.github.com>
2021-01-25 18:03:36 -0500

    Update to trollsmile-core@3

--- utils/types.ts
+++ utils/types.ts
@@ -1,6 +1,6 @@
-import { Message, TextChannel } from 'discord.js'
-import type Trollsmile from '..'
-type Bot = typeof Trollsmile
+import { Client, Message, TextChannel } from 'discord.js'
+import Trollsmile from 'trollsmile-core'
+
 type Return = Parameters<TextChannel['send']>[0]
 
 interface CommandObj {
@@ -10,10 +10,12 @@ interface CommandObj {
   aliases?: string[]
 }
 
-// interface Bot extends Client {
-//   commands: Collection<string, CommandObj>
-//   aliases: Collection<string, string>
-// }
+
+type Bot = Trollsmile<Message, CommandObj> & {
+  client: Client
+  getCommandName (cmdname: string): string | undefined
+}
+
 
 export {
   Bot, CommandObj
